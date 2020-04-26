958e263e0a54ad5d919800aba4195ef4d5fc1200
mice-k <60864882+mice-k@users.noreply.github.com>
2020-04-26 11:37:00 +0900

    hive support

--- main.js
+++ main.js
@@ -41,15 +41,71 @@ function transfer(urlParams) {
 	);
 };
 
+function hive_customJSON(urlParams) {
+	hive_keychain.requestCustomJson(
+		urlParams.get('username'),
+		urlParams.get('id'),
+		urlParams.get('auth'),
+		urlParams.get('json'),
+		urlParams.get('message'),
+		function(response) {
+			console.log('main js response - custom JSON');
+			console.log(response);
+		}
+	);
+};
+
+function hive_sendTokens(urlParams) {
+	hive_keychain.requestSendToken(
+		urlParams.get('username'),
+		urlParams.get('sendTo'),
+		urlParams.get('amount'),
+		urlParams.get('memo'),
+		urlParams.get('symbol'),
+		function(response) {
+			console.log("main js response - tokens");
+			console.log(response);
+		}
+	);
+};
+
+function hive_transfer(urlParams) {
+	hive_keychain.requestTransfer(
+		urlParams.get('username'),
+		urlParams.get('sendTo'),
+		urlParams.get('amount'),
+		urlParams.get('memo'),
+		urlParams.get('symbol'),
+		function(response) {
+			console.log("main js response - transfer");
+			console.log(response);
+		},
+		!(urlParams.get('enforce') === 'false')
+	);
+};
+
 window.addEventListener('load', function () {
 	const urlParams = new URLSearchParams(window.location.search);
-	const type = urlParams.get('type');
-	console.log(type);
-	if (type === 'customJSON') {
-		customJSON(urlParams);
-	} else if (type === 'transfer') {
-		transfer(urlParams);
-	} else if (type === 'sendTokens') {
-		sendTokens(urlParams);
-	};
+	const chain = urlParams.get('chain');
+	if (chain === 'steem') {
+		const type = urlParams.get('type');
+		console.log(type);
+		if (type === 'customJSON') {
+			customJSON(urlParams);
+		} else if (type === 'transfer') {
+			transfer(urlParams);
+		} else if (type === 'sendTokens') {
+			sendTokens(urlParams);
+		};
+	} else if (chain === 'hive') {
+		const type = urlParams.get('type');
+		console.log(type);
+		if (type === 'customJSON') {
+			hive_customJSON(urlParams);
+		} else if (type === 'transfer') {
+			hive_transfer(urlParams);
+		} else if (type === 'sendTokens') {
+			hive_sendTokens(urlParams);
+		};
+	}
 });
