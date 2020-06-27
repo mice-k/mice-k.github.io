b5c572377d2efd508ad457c12882217c3b90edfc
mice-k <60864882+mice-k@users.noreply.github.com>
2020-06-28 05:15:08 +0900

    bulk sell
    added experimental bulk sell function

--- cityhelper/main.js
+++ cityhelper/main.js
@@ -6,22 +6,22 @@ function hive_broadcast(username, operations, key) {
 		operations,
 		key,
 		function(response) {
-			console.log('main js response - broadcast');
-			console.log(response);
+			console.log('main js response - broadcast')
+			console.log(response)
 		}
-	);
+	)
 }
 
 async function heRPC(endpoint, method, params) {
-	let url = `https://api.hive-engine.com/rpc/${endpoint}`;
+	let url = `https://api.hive-engine.com/rpc/${endpoint}`
 	j = {'jsonrpc':'2.0', 'id':_id, 'method':method, 'params':params}
 	_id += 1
 	const response = await fetch(url, {
 		method: 'POST',
 		headers: {'Content-Type': 'application/json'},
 		body: JSON.stringify(j)
-	});
-	return response.json();
+	})
+	return response.json()
 }
 
 function heFind(contract, table, query, limit=1000, offset=0) {
@@ -32,18 +32,27 @@ function heFind(contract, table, query, limit=1000, offset=0) {
 		'limit':limit,
 		'offset':offset,
 		'indexes':[]
-	};
-	return heRPC('contracts', 'find', params);
+	}
+	return heRPC('contracts', 'find', params)
 }
 
-function groupCards(cards) {
-	let group = cards.reduce((h, obj) => Object.assign(h, { [obj.priceSymbol]:( h[obj.priceSymbol] || [] ).concat(obj.nftId.toString()) }), {})
+function groupSales(cards) {
+	const group = cards.reduce((h, obj) => Object.assign(h, { [obj.priceSymbol]:( h[obj.priceSymbol] || [] ).concat(obj.nftId.toString()) }), {})
 	const size = 50
-	var result = [];
+	let result = []
 	for (const [symbol, arr] of Object.entries(group)) {
 		for (var i = 0; i < arr.length; i += size) {
 			result.push(arr.slice(i, i+size))
 		}
+	}
+	return result
+}
+
+function groupCards(cards, size=50) {
+	const arr = cards.map(a => a._id.toString())
+	let result = []
+	for (var i = 0; i < arr.length; i += size) {
+		result.push(arr.slice(i, i+size))
 	}
 	return result
 }
@@ -58,16 +67,16 @@ function makeEngineOp(jdata, account) {
 }
 
 function getCancelOps(groups, account) {
-	let ops = [];
-	let jj = [];
+	let ops = []
+	let jj = []
 	let len = 2
 	for (var i = 0; i < groups.length; i++) {
 		let j = {
-			'contractName': 'nftmarket',
-			'contractAction': 'cancel',
+			'contractName':'nftmarket',
+			'contractAction':'cancel',
 			'contractPayload': {
-				'symbol': 'CITY',
-				'nfts': groups[i]
+				'symbol':'CITY',
+				'nfts':groups[i]
 			}
 		}
 		let l = JSON.stringify(j).length + 1
@@ -76,40 +85,110 @@ function getCancelOps(groups, account) {
 			len += l
 		} else {
 			ops.push(['custom_json', makeEngineOp(jj, account)])
+			if (ops.length > 4) {
+				return ops
+			}
 			len = 2 + l
-			jj = [];
+			jj = []
 		}
 	}
-	if (jj.length > 0) {
+	if ((jj.length > 0) & (ops.length < 5)) {
 		ops.push(['custom_json', makeEngineOp(jj, account)])
 	}
 	return ops
 }
 
+function getSellOps(groups, account, price, symbol) {
+	let ops = []
+	let jj = []
+	let len = 2
+	for (var i = 0; i < groups.length; i++) {
+		let j = {
+			'contractName': 'nftmarket',
+			'contractAction': 'sell',
+			'contractPayload': {
+				'symbol':'CITY',
+				'nfts':groups[i],
+				'price':price,
+				'priceSymbol':symbol,
+				'fee':250
+			}
+		}
+		let l = JSON.stringify(j).length + 1
+		if (len + l < 8192) {
+			jj.push(j)
+			len += l
+		} else {
+			ops.push(['custom_json', makeEngineOp(jj, account)])
+			if (ops.length > 4) {
+				return ops
+			}
+			len = 2 + l
+			jj = []
+		}
+	}
+	if ((jj.length > 0) & (ops.length < 5)) {
+		ops.push(['custom_json', makeEngineOp(jj, account)])
+	}
+	return ops
+}
+
+function displayOps(ops) {
+	let p = document.createElement('pre')
+	p.style.wordWrap = 'break-word'
+	p.style.whiteSpace = 'pre-wrap'
+	p.innerHTML = JSON.stringify(ops, null, '  ')
+	document.body.appendChild(p)
+}
+
 function cancel(urlParams) {
-	let account = urlParams.get('account')
-	query = {'account':account};
-	let promise = heFind('nftmarket', 'CITYsellBook', query);
+	const account = urlParams.get('account')
+	const card = urlParams.get('card')
+	if (card) {
+		query = {'account':account, 'grouping.name':card}
+	} else {
+		query = {'account':account}
+	}
+	const promise = heFind('nftmarket', 'CITYsellBook', query)
 	promise.then(function(result) {
-		let cards = result['result'];
-		let groups = groupCards(cards);
-		let ops = getCancelOps(groups, account)
+		const cards = result['result']
+		const groups = groupSales(cards)
+		const ops = getCancelOps(groups, account)
+		displayOps(ops)
 		hive_broadcast(account, ops, 'Active')
-	});
+	})
+}
+
+function sell(urlParams) {
+	const account = urlParams.get('account')
+	const card = urlParams.get('card')
+	const count = parseInt(urlParams.get('count'))
+	const price = urlParams.get('price')
+	const symbol = urlParams.get('symbol')
+	query = {'account':account, 'properties.name':card}
+	const promise = heFind('nft', 'CITYinstances', query)
+	promise.then(function(result) {
+		const cards = result['result'].reverse().slice(0, count)
+		console.log(cards)
+		const groups = groupCards(cards)
+		const ops = getSellOps(groups, account, price, symbol)
+		displayOps(ops)
+		hive_broadcast(account, ops, 'Active')
+	})
 }
 
 window.addEventListener('load', function () {
 	if (window.hive_keychain) {
-		keychains_ready();
+		keychains_ready()
 	} else {
 		var keychain_check = setInterval(function(){
 			if (window.hive_keychain) {
-				clearInterval(keychain_check);
-				keychains_ready();
+				clearInterval(keychain_check)
+				keychains_ready()
 			}
-		}, 250);
+		}, 250)
 	}
-});
+})
 
 function keychains_ready() {
 	const urlParams = new URLSearchParams(window.location.search);
@@ -117,8 +196,8 @@ function keychains_ready() {
 	if (command === 'transfer') {
 		
 	} else if (command === 'sell') {
-		
+		sell(urlParams)
 	} else if (command === 'cancel') {
-		cancel(urlParams);
+		cancel(urlParams)
 	}
 }
