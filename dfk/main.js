824d3b92bb19ef2bad4c554703e497f681770582
mice-k <60864882+mice-k@users.noreply.github.com>
2021-11-15 20:48:52 -0600

    dfk bot
    dfk bot

---
+++ dfk/main.js
@@ -0,0 +1,53 @@
+function enable() {
+	if (ethereum.selectedAddress) {
+		document.getElementById('connect-button').disabled = true
+		document.getElementById('disapprove-button').disabled = false
+		document.getElementById('approve-button').disabled = false
+		document.getElementById('address-label').innerHTML = ethereum.selectedAddress
+	}
+}
+
+async function connect() {
+	await ethereum.request({method: 'eth_requestAccounts'});
+	enable()
+}
+
+async function approve() {
+	const transactionParameters = {
+	  gasPrice: '0x3b9aca00',
+	  to: '0x5F753dcDf9b1AD9AabC1346614D1f4746fd6Ce5C',
+	  from: ethereum.selectedAddress,
+	  value: '0x0',
+	  data: '0xa22cb465000000000000000000000000c59346e7ece1fef9bca99872398ccbf0d6e4118b0000000000000000000000000000000000000000000000000000000000000001',
+	  chainId: '0x63564c40'
+	}
+	const txHash = await ethereum.request({
+	  method: 'eth_sendTransaction',
+	  params: [transactionParameters],
+	})
+	console.log(txHash)
+}
+
+async function disapprove() {
+	const transactionParameters = {
+	  gasPrice: '0x3b9aca00',
+	  to: '0x5F753dcDf9b1AD9AabC1346614D1f4746fd6Ce5C',
+	  from: ethereum.selectedAddress,
+	  value: '0x0',
+	  data: '0xa22cb465000000000000000000000000c59346e7ece1fef9bca99872398ccbf0d6e4118b0000000000000000000000000000000000000000000000000000000000000000',
+	  chainId: '0x63564c40'
+	}
+	const txHash = await ethereum.request({
+	  method: 'eth_sendTransaction',
+	  params: [transactionParameters],
+	})
+	console.log(txHash)
+}
+
+window.addEventListener('load', function () {
+	enable()
+	document.getElementById('connect-button').onclick = connect
+	document.getElementById('approve-button').onclick = approve
+	document.getElementById('disapprove-button').onclick = disapprove
+	console.log(ethereum.selectedAddress)
+})
